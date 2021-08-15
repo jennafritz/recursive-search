bc64b0f76b476ebba0639311f1accf556a4c35a3
jennafritz <jennafritz24@gmail.com>
2021-08-15 18:29:04 -0400

    Completed

--- ruby/recursive_search.rb
+++ ruby/recursive_search.rb
@@ -29,6 +29,16 @@ if __FILE__ == $PROGRAM_NAME
 end
 
 # Please add your pseudocode to this file
+  # until the target is found or array is empty
+  # check if the first element of the array is equal to the target
+  # if so, return true
+  # if not, slice the first element off of the array
+  # call the recursive function with the sliced array
 
 
 # And a written explanation of your solution
+  # we first check the two base cases:
+    # if the length of the array is less than 1 (since we're using a slicing approach)
+    # or if the target is equal to the value of the first element in the array
+  # if neither of those are true, we run the function again
+  # this time, with a sliced version of the array which removes the first element
